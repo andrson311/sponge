8e1e689da19d66d69a507401592b2e05fd3abed8
Andraž Kržišnik <andraz.krzisnik1993@gmail.com>
2026-05-26 03:07:56 +0200

    tutorial 4

--- test.cpp
+++ test.cpp
@@ -1,9 +1,36 @@
 #include <stdio.h>
+#include <string>
+#include <cstring>
 #include <GL/glew.h>
 #include <GL/freeglut.h>
 #include <glm/glm.hpp>
+#include <fstream>
+
+bool ReadFile(const char* pFileName, std::string& outFile);
 
 GLuint VBO;
+
+bool ReadFile(const char* pFileName, std::string& outFile) {
+    std::ifstream f(pFileName);
+
+    bool ret = false;
+
+    if (f.is_open()) {
+        std::string line;
+        while (getline(f, line)) {
+            outFile.append(line);
+            outFile.append("\n");
+        }
+
+        f.close();
+
+        ret = true;
+    } else {
+        printf("Error: '%s' is cooked\n", pFileName);
+    }
+
+    return ret;
+}
 
 static void RenderSceneCB()
 {
@@ -30,6 +57,7 @@ static void RenderSceneCB()
 static void CreateVertexBuffer() {
     glEnable(GL_CULL_FACE);
     glFrontFace(GL_CW); // GL_CCW by default
+    // glCullFace(GL_FRONT) // note: sets the cull face
 
     glm::vec3 Vertices[3];
 
@@ -40,6 +68,85 @@ static void CreateVertexBuffer() {
     glGenBuffers(1, &VBO);
     glBindBuffer(GL_ARRAY_BUFFER, VBO);
     glBufferData(GL_ARRAY_BUFFER, sizeof(Vertices), Vertices, GL_STATIC_DRAW);
+}
+
+static void AddShader(GLuint ShaderProgram, const char* pShaderText, GLenum ShaderType) {
+    GLuint ShaderObj = glCreateShader(ShaderType);
+
+    if (ShaderObj == 0) {
+        fprintf(stderr, "Error creating shader type %d\n", ShaderType);
+        exit(0);
+    }
+
+    const GLchar* p[1];
+    p[0] = pShaderText;
+
+    GLint Lengths[1];
+    Lengths[0] = (GLint)strlen(pShaderText);
+
+    glShaderSource(ShaderObj, 1, p, Lengths);
+    glCompileShader(ShaderObj);
+
+    GLint success;
+    glGetShaderiv(ShaderObj, GL_COMPILE_STATUS, &success);
+
+    if (!success) {
+        GLchar InfoLog[1024];
+        glGetShaderInfoLog(ShaderObj, 1024, NULL, InfoLog);
+        fprintf(stderr, "Error compiling shader type %d: '%s'\n", ShaderType, InfoLog);
+        exit(1);
+    }
+
+    glAttachShader(ShaderProgram, ShaderObj);
+}
+
+// names of the shader files
+const char* pVSFileName = "shader.vs";
+const char* pFSFileName = "shader.fs";
+
+static void CompileShaders() {
+    GLuint ShaderProgram = glCreateProgram();
+
+    if (ShaderProgram == 0) {
+        fprintf(stderr, "Error creating shader program\n");
+        exit(1);
+    }
+
+    std::string vs, fs;
+
+    if (!ReadFile(pVSFileName, vs)) {
+        exit(1);
+    }
+
+    AddShader(ShaderProgram, vs.c_str(), GL_VERTEX_SHADER);
+
+    if (!ReadFile(pFSFileName, fs)) {
+        exit(1);
+    }
+
+    AddShader(ShaderProgram, fs.c_str(), GL_FRAGMENT_SHADER);
+
+    GLint Success = 0;
+    GLchar ErrorLog[1024] = { 0 };
+
+    glLinkProgram(ShaderProgram);
+
+    glGetProgramiv(ShaderProgram, GL_LINK_STATUS, &Success);
+    if (Success == 0) {
+        glGetProgramInfoLog(ShaderProgram, sizeof(ErrorLog), NULL, ErrorLog);
+        fprintf(stderr, "Error linking shader program: '%s'\n", ErrorLog);
+        exit(1);
+    }
+
+    glValidateProgram(ShaderProgram);
+    glGetProgramiv(ShaderProgram, GL_VALIDATE_STATUS, &Success);
+    if (!Success) {
+        glGetProgramInfoLog(ShaderProgram, sizeof(ErrorLog), NULL, ErrorLog);
+        fprintf(stderr, "Invalid shader program: '%s'\n", ErrorLog);
+        exit(1);
+    }
+
+    glUseProgram(ShaderProgram);
 }
 
 int main(int argc, char** argv)
@@ -68,6 +175,8 @@ int main(int argc, char** argv)
     //glClearColor(Red, Green, Blue, Alpha);
 
     CreateVertexBuffer();
+
+    CompileShaders();
 
     glutDisplayFunc(RenderSceneCB);
 
